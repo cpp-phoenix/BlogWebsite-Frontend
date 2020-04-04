732e350dec3d5fddda1fad21a49f10f9a50209db
Gurjinder Sidhu <gurjindersr@gmail.com>
2020-04-04 22:49:58 +0530

    added codeowners and gitignore files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Landing from './Landing';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
