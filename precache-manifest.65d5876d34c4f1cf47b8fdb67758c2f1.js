5e4e07ec7000bfb532342b3712aae32b3298c903
Gurjinder Sidhu <gurjindersr@gmail.com>
2020-05-04 18:27:47 +0530

    Updates

---
+++ precache-manifest.65d5876d34c4f1cf47b8fdb67758c2f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be668c6c4ae326e0181225def70030a",
+    "url": "/BlogWebsite-Frontend/index.html"
+  },
+  {
+    "revision": "c69e2cf7434e774a260c",
+    "url": "/BlogWebsite-Frontend/static/css/2.5d4f1837.chunk.css"
+  },
+  {
+    "revision": "504b6a913d9b40df1b0e",
+    "url": "/BlogWebsite-Frontend/static/css/main.78f7994e.chunk.css"
+  },
+  {
+    "revision": "c69e2cf7434e774a260c",
+    "url": "/BlogWebsite-Frontend/static/js/2.176d38f9.chunk.js"
+  },
+  {
+    "revision": "84ea0b71b40343a457003ee66352b014",
+    "url": "/BlogWebsite-Frontend/static/js/2.176d38f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504b6a913d9b40df1b0e",
+    "url": "/BlogWebsite-Frontend/static/js/main.2de72213.chunk.js"
+  },
+  {
+    "revision": "66f17d590ad94ba4b464",
+    "url": "/BlogWebsite-Frontend/static/js/runtime-main.5599ea89.js"
+  }
+]);
